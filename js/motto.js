458401d4f622dad8532ebc4f1088243aa5108ad2
rudymemo <rudydiff@126.com>
2021-07-29 16:53:14 +0800

    add point

--- js/motto.js
+++ js/motto.js
@@ -1,10 +1,10 @@
 var mottos = [
-		{"author":"Somebody", "content":"慷概付出的，便是经常得到的"}
-		,{"author":"Somebody", "content":"善良比聪明重要"}
-		,{"author":"Somebody", "content":"没有终点，从各种意义上来说，其实是没有开始"}
+		{"author":"Somebody", "content":"慷概付出的，便是经常得到的。"}
+		,{"author":"Somebody", "content":"善良比聪明重要。"}
+		,{"author":"Somebody", "content":"没有终点，从各种意义上来说，其实是没有开始。"}
 		,{"author":"保罗·麦尔", "content":"你所清楚预见的、热切渴望的、真诚追求的、全心争取的，都会自然而然地实现。"}
-		,{"author":"Somebody", "content":"真正的救赎，并不是厮杀后的胜利，而是在苦难之中找到生的力量和心的安宁"}
-		,{"author":"Somebody", "content":"善始者众，善终者寡"}
-		,{"author":"Somebody", "content":"风吹枯叶落，落叶生肥土，肥土丰香果。孜孜不倦，不紧不慢，人生果实"}
-		,{"author":"Somebody", "content":"人生道路上总会有一些不期而遇的温暖给人以希望"}
+		,{"author":"Somebody", "content":"真正的救赎，并不是厮杀后的胜利，而是在苦难之中找到生的力量和心的安宁。"}
+		,{"author":"Somebody", "content":"善始者众，善终者寡。"}
+		,{"author":"Somebody", "content":"风吹枯叶落，落叶生肥土，肥土丰香果。孜孜不倦，不紧不慢，人生果实。"}
+		,{"author":"Somebody", "content":"人生道路上总会有一些不期而遇的温暖给人以希望。"}
 	]
